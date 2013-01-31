45d99f761bc3073389d9da13d2c7624a641b50aa
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2013-01-31 17:05:14 +0100

    buildsys: protobuf message library support
    Added support for libraries of messages (in addition to one library per
    message). This is particularly useful for messages that belong together
    and have dependencies.

--- etc/buildsys/protobuf.mk
+++ etc/buildsys/protobuf.mk
@@ -34,6 +34,7 @@ ifeq ($(HAVE_PROTOBUF),1)
 
 ifneq ($(PROTOBUF_all),)
   $(foreach P,$(PROTOBUF_all),							\
+	$(if $(filter-out %_msgs,$P),						\
 	$(eval LDFLAGS_protobuf_lib$P     = $(LDFLAGS_PROTOBUF))		\
 	$(eval OBJS_protobuf_lib$P        = $P.pb.o)				\
 	$(eval HDRS_interfaces_lib$P      = $P.pb.h)				\
@@ -43,20 +44,32 @@ ifneq ($(PROTOBUF_all),)
 	$(eval PROTOBUF_SRCS             += $(SRCDIR)/$P.pb.cpp)		\
 	$(eval PROTOBUF_HDRS             += $(SRCDIR)/$P.pb.h)			\
 	$(eval PROTOBUF_LIBS             += $(PROTOBUF_LIBDIR)/lib$P.$(SOEXT))	\
-	$(eval PROTOBUF_TOUCH            += $(SRCDIR)/$(OBJDIR)/$P.touch)	\
-	$(eval PROTOBUF_OBJS             += $P.pb.o)				\
-	$(eval LIBS_all                  += $$(PROTOBUF_LIBDIR)/lib$P.$(SOEXT))	\
+	$(eval LIBS_all                  += $(PROTOBUF_LIBDIR)/lib$P.$(SOEXT))	\
 	$(eval CLEAN_FILES               += $P.pb.h $P.pb.cpp)			\
+	, \
+	$(eval LDFLAGS_protobuf_lib$P     = $(LDFLAGS_PROTOBUF))		\
+	$(eval OBJS_protobuf_lib$P        = $(MSGS_$P:%=%.pb.o))		\
+	$(eval HDRS_interfaces_lib$P      = $(MSGS_$P:%=%.pb.h))		\
+	$(eval INST_LIB_SUBDIR_protobuf_lib$P   = $(FFLIBSUBDIR))		\
+	$(eval INST_HDRS_SUBDIR_protobuf_lib$P  = protobuf)			\
+	$(eval OBJS_all                  += $$(OBJS_protobuf_lib$P))		\
+	$(eval PROTOBUF_SRCS             += $(MSGS_$P:%=$(SRCDIR)/%.pb.cpp))	\
+	$(eval PROTOBUF_HDRS             += $(MSGS_$P:%=$(SRCDIR)/%.pb.h))	\
+	$(eval PROTOBUF_LIBS             += $(PROTOBUF_LIBDIR)/lib$P.$(SOEXT))	\
+	$(eval OBJS_lib$P.$(SOEXT)	  = $(MSGS_$P:%=%.pb.o))		\
+	$(eval LIBS_all                  += $(PROTOBUF_LIBDIR)/lib$P.$(SOEXT))	\
+	$(eval CLEAN_FILES               += $(MSGS_$P:%=%.pb.h) 		\
+					    $(MSGS_$P:%=%.pb.cpp))		\
+	)\
   )
 endif
 
 ifeq ($(OBJSSUBMAKE),1)
 
-$(PROTOBUF_SRCS): $(SRCDIR)/%.pb.cpp: $(SRCDIR)/$(OBJDIR)/%.touch
-$(PROTOBUF_HDRS): $(SRCDIR)/%.pb.h: $(SRCDIR)/$(OBJDIR)/%.touch
+$(PROTOBUF_SRCS): $(SRCDIR)/%.pb.cpp: $(SRCDIR)/$(OBJDIR)/%.pb.touch
+$(PROTOBUF_HDRS): $(SRCDIR)/%.pb.h: $(SRCDIR)/$(OBJDIR)/%.pb.touch
 
-
-$(PROTOBUF_TOUCH): $(SRCDIR)/$(OBJDIR)/%.touch: $(SRCDIR)/%.proto
+$(SRCDIR)/$(OBJDIR)/%.pb.touch: $(SRCDIR)/%.proto
 	$(SILENTSYMB) echo "$(INDENT_PRINT)--> Generating $* (Protobuf Message)"
 	$(SILENT)$(PROTOBUF_PROTOC) --cpp_out $(SRCDIR) --proto_path $(SRCDIR) $<
 	$(SILENT) mv $(SRCDIR)/$*.pb.cc $(SRCDIR)/$*.pb.cpp
@@ -66,6 +79,10 @@ $(PROTOBUF_TOUCH): $(SRCDIR)/$(OBJDIR)/%.touch: $(SRCDIR)/%.proto
 .SECONDARY: $(PROTOBUF_SRCS) $(PROTOBUF_HDRS)
 
 endif # OBJSSUBMAKE != 1
+
+$(PROTOBUF_LIBDIR)/lib%_msgs.so: $$(patsubst %.proto,%.pb.o,$$(MSGS_$$(call nametr,$$*)))
+	$(SILENT)echo $@ from $@
+
 
 ifneq ($(PLUGINS_all),)
 $(PLUGINS_all:%.$(SOEXT)=%.$(SOEXT)): | $(PROTOBUF_LIBS:%.$(SOEXT)=%.$(SOEXT))
