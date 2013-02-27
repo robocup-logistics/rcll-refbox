c9485ee446fecd9f1c9fb8d9cc9a954e4fab22dd
Daniel Ewert <daniel.ewert@ima-zlw-ifu.rwth-aachen.de>
2013-02-27 15:00:56 +0100

    llsfvis: main method to test behavior

--- src/llsfvis/llsfvis.cpp
+++ src/llsfvis/llsfvis.cpp
@@ -35,38 +35,53 @@
 
 #include <gtkmm.h>
 #include "MainWindow.h"
+#include "RefboxClient.h"
+#include "msgs/MachineSpec.pb.h"
 
 //using namespace Gtk;
 int main(int argc, char* argv[]) {
 	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv,
 			"org.gtkmm.examples.base");
 
-	LLSFVis::PlayField pf;
+	/*
+	 LLSFVis::PlayField pf;
 
-	pf.add_machine(3.92, 1.68, 0, "M1", "T0");
-	pf.add_machine(3.92, 3.92, 0, "M2", "T0");
-	pf.add_machine(2.80, 0.56, 0, "M3", "T0");
-	pf.add_machine(2.80, 1.68, 0, "M4", "T0");
-	pf.add_machine(2.80, 2.80, 0, "M5", "T0");
-	pf.add_machine(2.80, 3.92, 0, "M6", "T0");
-	pf.add_machine(2.80, 5.04, 0, "M7", "T0");
-	pf.add_machine(1.68, 1.68, 0, "M8", "T0");
-	pf.add_machine(1.68, 3.92, 0, "M9", "T0");
-	pf.add_machine(1.68, 5.04, 0, "M10", "T0");
+	 pf.add_machine(3.92, 1.68, 0, "M1", "T0");
+	 pf.add_machine(3.92, 3.92, 0, "M2", "T0");
+	 pf.add_machine(2.80, 0.56, 0, "M3", "T0");
+	 pf.add_machine(2.80, 1.68, 0, "M4", "T0");
+	 pf.add_machine(2.80, 2.80, 0, "M5", "T0");
+	 pf.add_machine(2.80, 3.92, 0, "M6", "T0");
+	 pf.add_machine(2.80, 5.04, 0, "M7", "T0");
+	 pf.add_machine(1.68, 1.68, 0, "M8", "T0");
+	 pf.add_machine(1.68, 3.92, 0, "M9", "T0");
+	 pf.add_machine(1.68, 5.04, 0, "M10", "T0");
 
-	pf.add_machine(5.40, 0.20, M_PI * 0.25, "R1", "RU");
-	pf.add_machine(0.20, 5.40, M_PI * 1.25, "R2", "RU");
-	pf.add_machine(5.40, 5.40, M_PI * 0.75, "T", "TU");
-
-	LLSFVis::GameState gs;
-	gs.setMachines(pf.getMachines());
-	gs.setPhase(LLSFVis::GameState::PRESTART);
+	 pf.add_machine(5.40, 0.20, M_PI * 0.25, "R1", "RU");
+	 pf.add_machine(0.20, 5.40, M_PI * 1.25, "R2", "RU");
+	 pf.add_machine(5.40, 5.40, M_PI * 0.75, "T", "TU");
+	 */
+	llsf_msgs::MachineSpecs mSpecs;
+	llsf_msgs::MachineSpec* m1 = mSpecs.add_machines();
+	m1->set_name("M1");
+	m1->set_type("T0");
+	llsf_msgs::Pose2D* pose = m1->mutable_pose();
+	pose->set_x(3.92);
+	pose->set_y(1.68);
+	pose->set_ori(0);
+	llsf_msgs::LightSpec* red = m1->add_lights();
+	red->set_color(llsf_msgs::LightColor::RED);
+	red->set_state(llsf_msgs::LightState::ON);
+	llsf_msgs::LightSpec* yellow = m1->add_lights();
+	yellow->set_color(llsf_msgs::LightColor::YELLOW);
+	yellow->set_state(llsf_msgs::LightState::ON);
+	llsf_msgs::LightSpec* green = m1->add_lights();
+	green->set_color(llsf_msgs::LightColor::GREEN);
+	green->set_state(llsf_msgs::LightState::BLINK);
 
 	std::list<const LLSFVis::Puck*> pucks;
 	LLSFVis::Puck* p = new LLSFVis::Puck(2.3, 2.3, "p1", "S0");
 	pucks.push_back(p);
-
-	gs.setPucks(pucks);
 
 	std::list<const LLSFVis::Robot*> bots;
 	LLSFVis::Robot* r1 = new LLSFVis::Robot("Tick");
@@ -82,15 +97,15 @@ int main(int argc, char* argv[]) {
 	r3->setLastHeard(time(NULL) - 5);
 	bots.push_back(r3);
 
-	gs.setRobots(bots);
-	gs.setScore(42);
-	gs.setTime(123);
-
 	LLSFVis::MainWindow mainWindow;
-	mainWindow.set_playfield(pf);
+	//LLSFVis::RefboxClient refboxClient(mainWindow);
+	//mainWindow.set_playfield(pf);
 	mainWindow.add_log_message("Good Morning");
-	mainWindow.set_attention_msg("ATTENTION! ROBOT'S GONNA BLOW UP!");
-	mainWindow.update_game_state(gs);
+	mainWindow.update_machines(mSpecs);
+	llsf_msgs::AttentionMessage msg;
+	msg.set_message("BEWARE, WE ARE GONNA BLOW UP!!");
+	msg.set_time_to_show(10);
+	mainWindow.set_attention_msg(msg);
 	return app->run(mainWindow);
 
 }
