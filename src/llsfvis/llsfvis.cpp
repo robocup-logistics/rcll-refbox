f49994a8bd56eb5801a56cfbef8170606826f10d
Daniel Ewert <daniel.ewert@ima-zlw-ifu.rwth-aachen.de>
2013-02-28 14:54:07 +0100

    llsfvis: using namespace llsf_msgs

--- src/llsfvis/llsfvis.cpp
+++ src/llsfvis/llsfvis.cpp
@@ -38,7 +38,7 @@
 #include "RefboxClient.h"
 #include "msgs/MachineSpec.pb.h"
 
-//using namespace Gtk;
+using namespace llsf_msgs;
 int main(int argc, char* argv[]) {
 	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv,
 			"org.gtkmm.examples.base");
@@ -61,48 +61,47 @@ int main(int argc, char* argv[]) {
 	 pf.add_machine(0.20, 5.40, M_PI * 1.25, "R2", "RU");
 	 pf.add_machine(5.40, 5.40, M_PI * 0.75, "T", "TU");
 	 */
-	llsf_msgs::MachineSpecs mSpecs;
-	llsf_msgs::MachineSpec* m1 = mSpecs.add_machines();
+	MachineSpecs mSpecs;
+	MachineSpec* m1 = mSpecs.add_machines();
 	m1->set_name("M1");
 	m1->set_type("T0");
-	llsf_msgs::Pose2D* pose = m1->mutable_pose();
+	Pose2D* pose = m1->mutable_pose();
 	pose->set_x(3.92);
 	pose->set_y(1.68);
 	pose->set_ori(0);
-	llsf_msgs::LightSpec* red = m1->add_lights();
-	red->set_color(llsf_msgs::LightColor::RED);
-	red->set_state(llsf_msgs::LightState::ON);
-	llsf_msgs::LightSpec* yellow = m1->add_lights();
-	yellow->set_color(llsf_msgs::LightColor::YELLOW);
-	yellow->set_state(llsf_msgs::LightState::ON);
-	llsf_msgs::LightSpec* green = m1->add_lights();
-	green->set_color(llsf_msgs::LightColor::GREEN);
-	green->set_state(llsf_msgs::LightState::BLINK);
+	LightSpec* red = m1->add_lights();
+	red->set_color(LightColor::RED);
+	red->set_state(LightState::ON);
+	LightSpec* yellow = m1->add_lights();
+	yellow->set_color(LightColor::YELLOW);
+	yellow->set_state(LightState::ON);
+	LightSpec* green = m1->add_lights();
+	green->set_color(LightColor::GREEN);
+	green->set_state(LightState::BLINK);
 
 	std::list<const LLSFVis::Puck*> pucks;
 	LLSFVis::Puck* p = new LLSFVis::Puck(2.3, 2.3, "p1", "S0");
 	pucks.push_back(p);
 
-	std::list<const LLSFVis::Robot*> bots;
-	LLSFVis::Robot* r1 = new LLSFVis::Robot("Tick");
-	r1->setPose(2.5, 2.5, 0);
-	r1->setLastHeard(time(NULL));
-	bots.push_back(r1);
-	LLSFVis::Robot* r2 = new LLSFVis::Robot("Trick");
-	r2->setPose(3.5, 3.5, M_PI_2);
-	r2->setLastHeard(time(NULL) - 3);
-	bots.push_back(r2);
-	LLSFVis::Robot* r3 = new LLSFVis::Robot("Track");
-	r3->setPose(0.5, 2.5, M_PI_2 / 2);
-	r3->setLastHeard(time(NULL) - 5);
-	bots.push_back(r3);
+	RobotInfo rInfo;
+	Robot* r = rInfo.add_robots();
+	r->set_name("Tick");
+	r->set_team("Carologistics");
+	Time* rtime = r->mutable_last_seen();
+	rtime->set_sec(3);
+	rtime->set_nsec(14);
+	Pose2D* rpose = r->mutable_pose();
+	rpose->set_x(2.8);
+	rpose->set_y(2.8);
+	rpose->set_ori(0);
 
 	LLSFVis::MainWindow mainWindow;
 	//LLSFVis::RefboxClient refboxClient(mainWindow);
 	//mainWindow.set_playfield(pf);
 	mainWindow.add_log_message("Good Morning");
 	mainWindow.update_machines(mSpecs);
-	llsf_msgs::AttentionMessage msg;
+	mainWindow.update_robots(rInfo);
+	AttentionMessage msg;
 	msg.set_message("BEWARE, WE ARE GONNA BLOW UP!!");
 	msg.set_time_to_show(10);
 	mainWindow.set_attention_msg(msg);
