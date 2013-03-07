8249436afd0af2d02dc95202217afa6e12131317
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2013-03-07 19:27:55 +0100

    refbox: add ExplorationInfo message types
    And include MachineCommands.pb.h for split-off messages.

--- src/refbox/refbox.cpp
+++ src/refbox/refbox.cpp
@@ -49,11 +49,13 @@
 #include <msgs/GameState.pb.h>
 #include <msgs/RobotInfo.pb.h>
 #include <msgs/MachineInfo.pb.h>
+#include <msgs/MachineCommands.pb.h>
 #include <msgs/AttentionMessage.pb.h>
 #include <msgs/OrderInfo.pb.h>
 #include <msgs/PuckInfo.pb.h>
 #include <msgs/MachineReport.pb.h>
 #include <msgs/VersionInfo.pb.h>
+#include <msgs/ExplorationInfo.pb.h>
 
 #include <google/protobuf/descriptor.h>
 #include <boost/bind.hpp>
@@ -244,6 +246,8 @@ LLSFRefBox::setup_protobuf_comm()
   mr_server.add_message_type<llsf_msgs::MachineReportEntry>();
   mr_server.add_message_type<llsf_msgs::MachineReport>();
   mr_server.add_message_type<llsf_msgs::MachineReportInfo>();
+  mr_server.add_message_type<llsf_msgs::ExplorationInfo>();
+  mr_server.add_message_type<llsf_msgs::ExplorationSignal>();
 
   MessageRegister &mr_peer = pbc_peer_->message_register();
   mr_peer.add_message_type<llsf_msgs::BeaconSignal>();
