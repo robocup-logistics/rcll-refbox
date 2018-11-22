601acfaf65da4d5221f38b4224c4c3d26c02aac4
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2018-11-22 15:43:28 +0000

    mongodb_log: also check Ubuntu's location for libmongoclient.so
    Ubuntu installs libraries in /usr/lib/${arc}/, e.g.,
    /usr/lib/x86_64-linux-gnu. We need to account for that when we look for
    libmongoclient.

--- src/libs/mongodb_log/mongodb.mk
+++ src/libs/mongodb_log/mongodb.mk
@@ -30,7 +30,7 @@ ifneq ($(wildcard /usr/include/mongo/client/dbclient.h /usr/local/include/mongo/
     LDFLAGS_MONGODB = -lm -lpthread \
 		      $(call boost-libs-ldflags,thread system filesystem) \
 		      $(LDFLAGS_LIBSSL)
-    ifneq ($(wildcard $(SYSROOT)/usr/lib/libmongoclient.so $(SYSROOT)/usr/lib64/libmongoclient.so $(SYSROOT)/usr/local/lib/libmongoclient.so $(SYSROOT)/usr/local/lib64/libmongoclient.so),)
+    ifneq ($(wildcard $(SYSROOT)/usr/lib/libmongoclient.so $(SYSROOT)/usr/lib64/libmongoclient.so $(SYSROOT)/usr/local/lib/libmongoclient.so $(SYSROOT)/usr/local/lib64/libmongoclient.so $(SYSROOT)/usr/lib/x86_64-linux-gnu/libmongoclient.so $(SYSROOT)/usr/local/lib/x86_64-linux-gnu/libmongoclient.so),)
       # we are linking against a shared library, add to link flags
       LDFLAGS_MONGODB += -lmongoclient
     endif
