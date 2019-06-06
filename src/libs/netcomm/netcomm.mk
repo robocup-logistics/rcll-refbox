b43fa5a9083f2b62108b8c66e730f9bc5fcfccc2
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2019-06-06 10:45:46 +0200

    netcomm: optionally disable avahi support
    Using avahi does not work properly in a docker container, allow
    disabling avahi support at build time by setting USE_AVAHI=0.

--- src/libs/netcomm/netcomm.mk
+++ src/libs/netcomm/netcomm.mk
@@ -13,20 +13,23 @@
 #
 #*****************************************************************************
 
-ifneq ($(PKGCONFIG),)
-  HAVE_AVAHI     := $(if $(shell $(PKGCONFIG) --exists 'avahi-client'; echo $${?/1/}),1,0)
-  HAVE_LIBCRYPTO := $(if $(shell $(PKGCONFIG) --exists 'libcrypto'; echo $${?/1/}),1,0)
-  LIBCRYPTO_PKG  := libcrypto
-  ifneq ($(HAVE_LIBCRYPTO),1)
-    HAVE_LIBCRYPTO := $(if $(shell $(PKGCONFIG) --exists 'openssl'; echo $${?/1/}),1,0)
-    LIBCRYPTO_PKG  := openssl
+USE_AVAHI ?= 1
+
+ifeq ($(USE_AVAHI),1)
+  ifneq ($(PKGCONFIG),)
+    HAVE_AVAHI     := $(if $(shell $(PKGCONFIG) --exists 'avahi-client'; echo $${?/1/}),1,0)
+    HAVE_LIBCRYPTO := $(if $(shell $(PKGCONFIG) --exists 'libcrypto'; echo $${?/1/}),1,0)
+    LIBCRYPTO_PKG  := libcrypto
+    ifneq ($(HAVE_LIBCRYPTO),1)
+      HAVE_LIBCRYPTO := $(if $(shell $(PKGCONFIG) --exists 'openssl'; echo $${?/1/}),1,0)
+      LIBCRYPTO_PKG  := openssl
+    endif
+  endif
+  ifeq ($(HAVE_AVAHI),1)
+    CFLAGS_AVAHI  = -DHAVE_AVAHI $(shell $(PKGCONFIG) --cflags avahi-client)
+    LDFLAGS_AVAHI = $(shell $(PKGCONFIG) --libs avahi-client)
+  endif
+  ifeq ($(HAVE_LIBCRYPTO),1)
+    CFLAGS += -DHAVE_LIBCRYPTO
   endif
 endif
-ifeq ($(HAVE_AVAHI),1)
-  CFLAGS_AVAHI  = -DHAVE_AVAHI $(shell $(PKGCONFIG) --cflags avahi-client)
-  LDFLAGS_AVAHI = $(shell $(PKGCONFIG) --libs avahi-client)
-endif
-ifeq ($(HAVE_LIBCRYPTO),1)
-  CFLAGS += -DHAVE_LIBCRYPTO
-endif
-
