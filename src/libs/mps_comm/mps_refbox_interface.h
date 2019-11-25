f1aebd30f7271db7681ccb8b2049c5945dc80f61
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2019-11-25 11:07:58 +0100

    mps_comm: add license headers to all files

--- src/libs/mps_comm/mps_refbox_interface.h
+++ src/libs/mps_comm/mps_refbox_interface.h
@@ -1,3 +1,24 @@
+/***************************************************************************
+ *  mps_refbox_interface.h - Thread handling for MPS stations
+ *
+ *  Created: Tue 24 Feb 2015 16:03:26 CET 16:03
+ *  Copyright  2015  David Masternak <david.masternak@alumni.fh-aachen.de>
+ *             2019  Till Hofmann <hofmann@kbsg.rwth-aachen.de>
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
 /*!
 * \file mps_refbox_interface.h
 * \brief Definition of communication between Refbox and Modbus server
--- src/libs/mps_comm/ring_station.cpp
+++ src/libs/mps_comm/ring_station.cpp
@@ -1,3 +1,24 @@
+/***************************************************************************
+ *  ring_station.cpp - OPC-UA communication with the RS
+ *
+ *  Created: Thu 21 Feb 2019 13:29:11 CET 13:29
+ *  Copyright  2019  Alex Maestrini <maestrini@student.tugraz.at>
+ *                   Till Hofmann <hofmann@kbsg.rwth-aachen.de>
+ ****************************************************************************/
+
+/*  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  Read the full text in the LICENSE.GPL file in the doc directory.
+ */
+
 #include "ring_station.h"
 
 #include "mps_io_mapping.h"
