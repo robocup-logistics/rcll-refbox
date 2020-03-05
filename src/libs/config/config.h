2c5df9ecf5abe46b9a2d92430b2fb2f5e71585b3
Tarik Viehmann <tarik.viehmann@rwth-aachen.de>
2020-03-05 16:05:02 +0100

    libs/config: update config from fawkes repo
    The config lib to parse yaml files was initially taken from the fawkes
    repo. Since then, improvements were made in fakwes, such as handling
    the parsing of empty lists. Hence, apply those changes to the refbox rep
    as well.

--- src/libs/config/config.h
+++ src/libs/config/config.h
@@ -117,20 +117,35 @@ public:
 
 	virtual bool is_default(const char *path) = 0;
 
-	virtual float                     get_float(const char *path)           = 0;
-	virtual unsigned int              get_uint(const char *path)            = 0;
-	virtual int                       get_int(const char *path)             = 0;
-	virtual bool                      get_bool(const char *path)            = 0;
-	virtual std::string               get_string(const char *path)          = 0;
-	virtual std::vector<float>        get_floats(const char *path)          = 0;
-	virtual std::vector<unsigned int> get_uints(const char *path)           = 0;
-	virtual std::vector<int>          get_ints(const char *path)            = 0;
-	virtual std::vector<bool>         get_bools(const char *path)           = 0;
-	virtual std::vector<std::string>  get_strings(const char *path)         = 0;
-	virtual ValueIterator *           get_value(const char *path)           = 0;
-	virtual std::string               get_type(const char *path)            = 0;
-	virtual std::string               get_comment(const char *path)         = 0;
-	virtual std::string               get_default_comment(const char *path) = 0;
+	virtual float        get_float(const char *path)  = 0;
+	virtual unsigned int get_uint(const char *path)   = 0;
+	virtual int          get_int(const char *path)    = 0;
+	virtual bool         get_bool(const char *path)   = 0;
+	virtual std::string  get_string(const char *path) = 0;
+	virtual float        get_float_or_default(const char *path, const float &default_val);
+	virtual unsigned int get_uint_or_default(const char *path, const unsigned int &default_val);
+	virtual int          get_int_or_default(const char *path, const int &default_val);
+	virtual bool         get_bool_or_default(const char *path, const bool &default_val);
+	virtual std::string  get_string_or_default(const char *path, const std::string &default_val);
+	virtual std::vector<float>        get_floats(const char *path)  = 0;
+	virtual std::vector<unsigned int> get_uints(const char *path)   = 0;
+	virtual std::vector<int>          get_ints(const char *path)    = 0;
+	virtual std::vector<bool>         get_bools(const char *path)   = 0;
+	virtual std::vector<std::string>  get_strings(const char *path) = 0;
+	virtual std::vector<float>        get_floats_or_defaults(const char *              path,
+	                                                         const std::vector<float> &default_val);
+	virtual std::vector<unsigned int>
+	                          get_uints_or_defaults(const char *path, const std::vector<unsigned int> &default_val);
+	virtual std::vector<int>  get_ints_or_defaults(const char *            path,
+	                                               const std::vector<int> &default_val);
+	virtual std::vector<bool> get_bools_or_defaults(const char *             path,
+	                                                const std::vector<bool> &default_val);
+	virtual std::vector<std::string>
+	                       get_strings_or_defaults(const char *path, const std::vector<std::string> &default_val);
+	virtual ValueIterator *get_value(const char *path)           = 0;
+	virtual std::string    get_type(const char *path)            = 0;
+	virtual std::string    get_comment(const char *path)         = 0;
+	virtual std::string    get_default_comment(const char *path) = 0;
 
 	virtual void set_float(const char *path, float f)                         = 0;
 	virtual void set_uint(const char *path, unsigned int uint)                = 0;
@@ -170,6 +185,253 @@ public:
 	virtual void unlock()   = 0;
 
 	virtual void try_dump() = 0;
+
+	/// @cond CONVENIENCE_METHODS
+	virtual bool
+	exists(const std::string &path)
+	{
+		return exists(path.c_str());
+	}
+
+	virtual bool
+	is_float(const std::string &path)
+	{
+		return is_float(path.c_str());
+	}
+	virtual bool
+	is_uint(const std::string &path)
+	{
+		return is_uint(path.c_str());
+	}
+	virtual bool
+	is_int(const std::string &path)
+	{
+		return is_int(path.c_str());
+	}
+	virtual bool
+	is_bool(const std::string &path)
+	{
+		return is_bool(path.c_str());
+	}
+	virtual bool
+	is_string(const std::string &path)
+	{
+		return is_string(path.c_str());
+	}
+	virtual bool
+	is_list(const std::string &path)
+	{
+		return is_list(path.c_str());
+	}
+
+	virtual bool
+	is_default(const std::string &path)
+	{
+		return is_default(path.c_str());
+	}
+
+	virtual float
+	get_float(const std::string &path)
+	{
+		return get_float(path.c_str());
+	}
+	virtual unsigned int
+	get_uint(const std::string &path)
+	{
+		return get_uint(path.c_str());
+	}
+	virtual int
+	get_int(const std::string &path)
+	{
+		return get_int(path.c_str());
+	}
+	virtual bool
+	get_bool(const std::string &path)
+	{
+		return get_bool(path.c_str());
+	}
+	virtual std::string
+	get_string(const std::string &path)
+	{
+		return get_string(path.c_str());
+	}
+	virtual std::vector<float>
+	get_floats(const std::string &path)
+	{
+		return get_floats(path.c_str());
+	}
+	virtual std::vector<unsigned int>
+	get_uints(const std::string &path)
+	{
+		return get_uints(path.c_str());
+	}
+	virtual std::vector<int>
+	get_ints(const std::string &path)
+	{
+		return get_ints(path.c_str());
+	}
+	virtual std::vector<bool>
+	get_bools(const std::string &path)
+	{
+		return get_bools(path.c_str());
+	}
+	virtual std::vector<std::string>
+	get_strings(const std::string &path)
+	{
+		return get_strings(path.c_str());
+	}
+	virtual ValueIterator *
+	get_value(const std::string &path)
+	{
+		return get_value(path.c_str());
+	}
+	virtual std::string
+	get_type(const std::string &path)
+	{
+		return get_type(path.c_str());
+	}
+	virtual std::string
+	get_comment(const std::string &path)
+	{
+		return get_comment(path.c_str());
+	}
+	virtual std::string
+	get_default_comment(const std::string &path)
+	{
+		return get_default_comment(path.c_str());
+	}
+
+	virtual void
+	set_float(const std::string &path, float f)
+	{
+		set_float(path.c_str(), f);
+	}
+	virtual void
+	set_uint(const std::string &path, unsigned int uint)
+	{
+		set_uint(path.c_str(), uint);
+	}
+	virtual void
+	set_int(const std::string &path, int i)
+	{
+		set_int(path.c_str(), i);
+	}
+	virtual void
+	set_bool(const std::string &path, bool b)
+	{
+		set_bool(path.c_str(), b);
+	}
+	virtual void
+	set_string(const std::string &path, std::string &s)
+	{
+		set_string(path.c_str(), s);
+	}
+	virtual void
+	set_string(const std::string &path, const char *s)
+	{
+		set_string(path.c_str(), s);
+	}
+	virtual void
+	set_floats(const std::string &path, std::vector<float> &f)
+	{
+		set_floats(path.c_str(), f);
+	}
+	virtual void
+	set_uints(const std::string &path, std::vector<unsigned int> &uint)
+	{
+		set_uints(path.c_str(), uint);
+	}
+	virtual void
+	set_ints(const std::string &path, std::vector<int> &i)
+	{
+		set_ints(path.c_str(), i);
+	}
+	virtual void
+	set_bools(const std::string &path, std::vector<bool> &b)
+	{
+		set_bools(path.c_str(), b);
+	}
+	virtual void
+	set_strings(const std::string &path, std::vector<std::string> &s)
+	{
+		set_strings(path.c_str(), s);
+	}
+	virtual void
+	set_strings(const std::string &path, std::vector<const char *> &s)
+	{
+		set_strings(path.c_str(), s);
+	}
+	virtual void
+	set_comment(const std::string &path, const char *comment)
+	{
+		set_comment(path.c_str(), comment);
+	}
+	virtual void
+	set_comment(const std::string &path, std::string &comment)
+	{
+		set_comment(path.c_str(), comment);
+	}
+
+	virtual void
+	erase(const std::string &path)
+	{
+		erase(path.c_str());
+	}
+
+	virtual void
+	set_default_float(const std::string &path, float f)
+	{
+		set_default_float(path.c_str(), f);
+	}
+	virtual void
+	set_default_uint(const std::string &path, unsigned int uint)
+	{
+		set_default_uint(path.c_str(), uint);
+	}
+	virtual void
+	set_default_int(const std::string &path, int i)
+	{
+		set_default_int(path.c_str(), i);
+	}
+	virtual void
+	set_default_bool(const std::string &path, bool b)
+	{
+		set_default_bool(path.c_str(), b);
+	}
+	virtual void
+	set_default_string(const std::string &path, std::string &s)
+	{
+		set_default_string(path.c_str(), s);
+	}
+	virtual void
+	set_default_string(const std::string &path, const char *s)
+	{
+		set_default_string(path.c_str(), s);
+	}
+
+	virtual void
+	set_default_comment(const std::string &path, const char *comment)
+	{
+		set_default_comment(path.c_str(), comment);
+	}
+	virtual void
+	set_default_comment(const std::string &path, std::string &comment)
+	{
+		set_default_comment(path.c_str(), comment);
+	}
+
+	virtual void
+	erase_default(const std::string &path)
+	{
+		erase_default(path.c_str());
+	}
+
+	virtual ValueIterator *
+	search(const std::string &path)
+	{
+		return search(path.c_str());
+	}
+	/// @endcond
 };
 
 } // end namespace llsfrb
