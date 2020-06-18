30b83e4bf30ac6fa5c26d1723e5ac55487f738ab
Daniel Swoboda <swoboda@kbsg.rwth-aachen.de>
2020-06-18 19:59:43 +0200

    libs/websocket: add check to prevent all clients from sending commands

--- src/libs/websocket/client.cpp
+++ src/libs/websocket/client.cpp
@@ -55,7 +55,8 @@ ClientWS::ClientWS(std::shared_ptr<boost::beast::websocket::stream<tcp::socket>>
 	data_     = data;
 	can_send_ = can_send;
 	socket->accept();
-	client_t = std::thread(&Client::receive_thread, this);
+	if(can_send)
+		client_t = std::thread(&Client::receive_thread, this);
 	logger_->log_info("Websocket", "client receive thread started");
 }
 
@@ -133,8 +134,8 @@ ClientS::ClientS(std::shared_ptr<tcp::socket> socket, Logger *logger, Data *data
 {
 	logger_   = logger;
 	data_     = data;
-	can_send_ = can_send;
-	client_t  = std::thread(&Client::receive_thread, this);
+	if(can_send)
+		client_t  = std::thread(&Client::receive_thread, this);
 	logger_->log_info("Websocket", "TCP-socket client receive thread started");
 }
 
